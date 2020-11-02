290070cd5925838303a93a6d8d94f3c4c63d7b49
GlitchR 9506 <GlitchR9506@gmail.com>
2020-11-01 21:01:41 -0500

    Rustboro 01

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -174,6 +174,7 @@
 #define MAP_RUSTBORO_CITY_FLAT2_2F          (14 | (11 << 8))
 #define MAP_RUSTBORO_CITY_FLAT2_3F          (15 | (11 << 8))
 #define MAP_RUSTBORO_CITY_HOUSE3            (16 | (11 << 8))
+#define MAP_TRAIN                           (17 | (11 << 8))
 
 // Map Group 12
 #define MAP_FORTREE_CITY_HOUSE1            (0 | (12 << 8))
