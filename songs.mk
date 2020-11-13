aabbe843c4bde7529c5f899907d6adbf954a7676
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2020-11-13 22:44:47 +0700

    add pullthetrigger in ROM
    notes: the song has to be added to ld_script otherwise it'll complain about ignored rodata or something

--- songs.mk
+++ songs.mk
@@ -577,7 +577,7 @@ $(MID_SUBDIR)/mus_encounter_elite_four.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G096 -V078
 
 $(MID_SUBDIR)/mus_pullthetrigger.s: %.s: %.mid
-	$(MID) $< $@ -E -R$(STD_REVERB) -G000 -V078
+	$(MID) $< $@ -E -R$(STD_REVERB) -G000 -V100
 
 $(MID_SUBDIR)/mus_c_vs_legend_beast.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -V080
